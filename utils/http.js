14451e30c35d879f0ee1d6a942c407d3d9ee3b65
Mustafa Zırhlı <mustafazirhli98@gmail.com>
2024-04-09 18:47:22 +0300

    fixed

--- utils/http.js
+++ utils/http.js
@@ -1,15 +1,21 @@
 import { axios } from "axios"
 
-const BASE_URL = ""
+const BASE_URL = "https://expensetracker-reactnati-dd53b-default-rtdb.firebaseio.com/"
 
-export const fetchData = () => {
+
+export const storeData = async (expensesData) => {
+    const response = await axios.get(BASE_URL + "expenses.json", expensesData)
+    console.log(response, "111111111111")
+}
+
+export const fetchData = async () => {
 
 }
 
-export const updateData = () => {
+export const updateData = async () => {
 
 }
 
-export const deleteData = () => {
+export const deleteData = async () => {
 
 }
